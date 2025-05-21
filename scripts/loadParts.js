bdc8ad83de35358cf3ef5e01c01ca2eb91fd4e2b
emptygxd <arsenii_rusin@mail.ru>
2025-05-21 03:37:35 +0300

    unnecessary script removed

--- scripts/loadParts.js
+++
@@ -1,9 +0,0 @@
-fetch("../modal.html")
-  .then((res) => {
-    if (!res.ok) throw new Error(`Error ${res.status}`);
-    return res.text();
-  })
-  .then((html) => {
-    document.getElementById("authModal").innerHTML = html;
-  })
-  .catch((err) => console.error(err));
